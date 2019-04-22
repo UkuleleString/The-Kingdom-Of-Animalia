015efb2a6fddda3c5253aa49dc098bdd4ff041ee
UkuleleString <pixeled241@gmail.com>
2019-04-22 12:35:21 -0400

    -Added Save Games

--- The Kingdom of Animalia.py
+++ The Kingdom of Animalia.py
@@ -1,6 +1,7 @@
 import Entities as Ent
 import time
 import os
+import pickle
 
 while True:
     print("********************")
@@ -13,12 +14,13 @@ while True:
     print()
     print("Type 'Leaderboard' to look at your top scores!")
     print()
-    print("Type 'info' to check how to play!")
+    print("Type 'Load' to load a save game!")
     print()
     print("Please enter one:")
     print()
     viewScreen = input()
     if viewScreen.lower() == "start":
+        p1 = Ent.player.player()
         os.system('clear')
         break
     elif viewScreen.lower() == "leaderboard":
@@ -26,17 +28,24 @@ while True:
         print()
         print()
         pass
-    elif viewScreen.lower() == "info":
+    elif viewScreen.lower() == "load":
         os.system('clear')
-        pass
+        with open('playerData.pkl', 'rb') as playerData:
+            p1 = pickle.load(playerData)
+        with open('worldData.pkl', 'rb') as worldData:
+            currentWorld = pickle.load(worldData)
+            print("Welcome back, " + p1.name + "!")
+            p1.printStats()
+            time.sleep(5)
+        break
     else:
-        print("Bitch, this is the fucking title screen. How the fuck you fuckin up already.")
+        print(
+            "Bitch, this is the fucking title screen. How the fuck you fuckin up already.")
         time.sleep(5)
         os.system('clear')
         pass
 
-"""   Start Screen    """
-p1 = Ent.player.player()
+"""   Starting the Game   """
 os.system('clear')
 
 
@@ -44,9 +53,9 @@ class world(object):
     '''"World" Class for subclass worlds
     Sets Level Gate, Boss Levels, '''
 
-    def __init__(self, lG, bL):
-        self.levelGate = lG
-        self.bossLevel = bL
+    def __init__(self, levelGate, bossLevel):
+        self.levelGate = levelGate
+        self.bossLevel = bossLevel
 
 
 class Eden(world):
@@ -74,13 +83,53 @@ theory'''
         super().__init__(6, 12)
         self.worldName = "Agartha"
 
+# Starts player in Midgard
+# Sets all quests to uncompleted
+if viewScreen.lower() == 'start':
+    currentWorld = Midgard()
+    questCompletion = [0]
 
-currentWorld = Midgard()
 while True:
     print("You have moved to " + currentWorld.worldName)
 
     while currentWorld.worldName == "Midgard":
-        pass
+        print("What would you like to do?")
+        playerAction = input()
+        if playerAction.lower() == "help":
+            os.system('clear')
+            print("Put info here")
+            print()
+            print("*******************************************************")
+            print()
+            print("Go to the 'Tavern' to pick up rumors or talk to patrons")
+            print("Go to the 'Dungeon' to level up or find items")
+            print("Go to the 'Shop' to use your gold to buy equipment")
+            print("Use the 'Portal' to go to other worlds")
+            print("Use 'save' to save the game")
+        elif playerAction.lower() == "Tavern":
+            os.system('clear')
+            pass
+        elif playerAction.lower() == "Dungeon":
+            os.system('clear')
+            pass
+        elif playerAction.lower() == "Shop":
+            os.system('clear')
+            pass
+        elif playerAction.lower() == "Portal":
+            os.system('clear')
+            print('')
+            print("Where would you like to go?")
+            transferWorld = input()
+            if transferWorld.lower() == 'eden' & p1.level >= Eden.levelGate:
+                print('')
+                pass
+        elif playerAction.lower() == "save":
+            os.system('clear')
+            with open('playerData.pkl', 'wb') as playerData:
+                pickle.dump(p1, playerData)
+            with open('worldData.pkl', 'wb') as worldData:
+                pickle.dump(currentWorld, worldData)
+                print('Game Saved!')
 
     while currentWorld.worldName == "Eden":
         pass
