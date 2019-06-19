8df3a45ab99ff5b03c65f86f367f86b50a75b4ec
David Cosio <dlccosio241@gmail.com>
2019-06-18 20:50:07 -0400

    - Keeps track in object rather than in package

--- gameFunctions/timeEvents.py
+++ gameFunctions/timeEvents.py
@@ -2,11 +2,9 @@ class timer(object):
     """Creates timer to return values based on
 what period of the day it is"""
 
-    self.hours = 0
-    self.minutes = 0
-
     def __init__(self):
         self.minutes = 15
+        self.hours = 0
 
     def checktime(self):
         hour = int(self.minutes / 60)
